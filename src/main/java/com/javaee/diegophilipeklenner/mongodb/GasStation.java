150edf501dbe23178ad10b76bc929c57fad8a6ee
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-07-12 18:04:41 -0300

    Initial commit

---
+++ src/main/java/com/javaee/diegophilipeklenner/mongodb/GasStation.java
@@ -0,0 +1,16 @@
+package com.javaee.diegophilipeklenner.mongodb;
+
+package com.javaee.mongodb.domain;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GasStation {
+	
+	private String id = UUID.randomUUID().toString();
+    private String name;
+}
